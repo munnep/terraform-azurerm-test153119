c23df21ac48d41b126656457316ca06d52cc2804
munnep <patrick.munne@hashicorp.com>
2024-07-02 12:33:48 +0200

    added terraform to the module

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ resource "azurerm_resource_group" "example" {
   name     = "example-patrick153119"
   location = "West Europe"
 }
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "tfe66.aws.munnep.com/test/azurerm"
+      version = "3.107.0"
+    }
+  }
+}
